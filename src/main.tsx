f3dc4b76fed38d4ff287503063659ce00ee79a21
dhruvil-sh <dhruvil@saleshandy.com>
2025-01-06 04:34:23 +0530

    fix: type error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import Root from "./root.tsx";
-import store from "./store";
 import "./index.css";
-import { history } from "./utils/history";
 
 createRoot(document.getElementById("root")!).render(<Root />);
